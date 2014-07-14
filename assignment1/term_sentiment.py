6c13576f599663978531621e7757fa70a8017cb0
Jai Chaudhary <jai.chaudhary.iitd@gmail.com>
2014-07-14 08:49:17 +0400

    Completed Assignment 1 -part 1-3 and my logic for assigning term_sentiment worked very well

--- assignment1/term_sentiment.py
+++ assignment1/term_sentiment.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 def hw():
     print 'Hello, world!'
@@ -9,9 +10,37 @@ def lines(fp):
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    scores = {} # initialize an empty dictionary
+    for line in sent_file:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  # Convert the score to an integer.
+
+    # print scores.items() # Print every (term, score) pair in the dictionary
+
+    non_sentiment_term_dict = {}
+    for line in tweet_file:
+        tweet_object  = json.loads(line)
+        textkey = "text"
+        if textkey in tweet_object.keys():
+            sentiment_term_dict_per_tweet = {}
+            non_sentiment_term_dict_per_tweet = {}
+            for tweet_term in tweet_object[textkey].encode('utf-8').split():
+                if tweet_term in scores.keys():
+                    sentiment_term_dict_per_tweet[tweet_term] = scores[tweet_term]
+            # print sentiment_term_dict_per_tweet
+            for tweet_term in tweet_object[textkey].encode('utf-8').split():
+                if tweet_term not in scores.keys():
+                    non_sentiment_term_dict_per_tweet[tweet_term] = float(sum(sentiment_term_dict_per_tweet.values()))/len(sentiment_term_dict_per_tweet.values())
+        for non_sentiment_term in non_sentiment_term_dict_per_tweet.keys():
+            if non_sentiment_term in non_sentiment_term_dict.keys():
+                non_sentiment_term_dict[non_sentiment_term].append(non_sentiment_term_dict_per_tweet[non_sentiment_term])
+            else:
+                non_sentiment_term_dict[non_sentiment_term] = [non_sentiment_term_dict_per_tweet[non_sentiment_term]]
+
+    # print non_sentiment_term_dict
+    for non_sentiment_term in non_sentiment_term_dict.keys():
+        print non_sentiment_term + " " + str(float(sum(non_sentiment_term_dict[non_sentiment_term]))/len(non_sentiment_term_dict[non_sentiment_term]))
 
 if __name__ == '__main__':
     main()
--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 def hw():
     print 'Hello, world!'
@@ -9,9 +10,34 @@ def lines(fp):
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    # hw()
+    # lines(sent_file)
+    # lines(tweet_file)
+
+    scores = {} # initialize an empty dictionary
+    for line in sent_file:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = int(score)  # Convert the score to an integer.
+
+    # print scores.items() # Print every (term, score) pair in the dictionary
+
+    for line in tweet_file:
+        tweet_object  = json.loads(line)
+        tweet_sentiment = 0
+
+        textkey = "text"
+        if textkey in tweet_object.keys():
+            for tweet_term in tweet_object[textkey].encode('utf-8').split():
+                if tweet_term in scores.keys():
+                    # print tweet_term, scores[tweet_term]
+                    tweet_sentiment += scores[tweet_term]
+        print str(tweet_sentiment)
+
+    
+
+    # print scores.items() # Print every (term, score) pair in the dictionary
+
+    # print json.loads(tweet_file)
 
 if __name__ == '__main__':
     main()
